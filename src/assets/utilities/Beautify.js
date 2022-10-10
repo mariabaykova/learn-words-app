ccbedb45a4f2ea6a492eec89bdb1823cd2d134a3
Maria Baykova <mnbaikova@gmail.com>
2022-10-10 16:02:31 +0500

    Updates

--- src/assets/utilities/Beautify.js
+++
@@ -1,8 +0,0 @@
-// почистить поля ввода от лишних символов
-// пока придумала удалять только лишние пробелы
-// пока не используется
-export default class Beautify {
-  static deleteOddSymbols(str) {
-    return str.replace(/ +/g, " ").replace(/^ +/, "").replace(/ +$/, "");
-  }
-}
