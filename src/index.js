dd28df8d4a78998cabcea9cfbe03c5025c645520
Maria Baykova <mnbaikova@gmail.com>
2022-10-28 16:05:42 +0500

    new version of context - doesn't work

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { WordsContextProvider } from "./assets/components/Context";
+// import { WordsContextProvider } from "./assets/components/Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   // <React.StrictMode>
-  <WordsContextProvider>
-    <App />
-  </WordsContextProvider>
+  // <WordsContextProvider>
+  <App />
+  // </WordsContextProvider>
   // </React.StrictMode>
 );
